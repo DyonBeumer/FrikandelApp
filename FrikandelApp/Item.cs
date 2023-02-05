5555423ede61a554becfce97d281df075b80e987
Dyon <dyonunic@gmaill.com>
2023-02-05 11:35:44 +0100

    ef benchmark added

--- FrikandelApp/Item.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FrikandelApp
-{
-    public class Item
-    {
-        public string Name { get; set; }
-        public decimal Price { get; set; }
-    }
-}
--- FrikandelApp/Person.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FrikandelApp
-{
-    public class Person
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
