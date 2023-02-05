57e9d00ecd19da64f3405d491e66b7f760d7b0ad
Dyon <dyonunic@gmaill.com>
2023-02-05 20:36:28 +0100

    now it compiles

--- Dapper.Snackbar/Order.cs
+++ Dapper.Snackbar/Order.cs
@@ -2,7 +2,7 @@
 
 public class Order
 {
-    public int Id { ge``````t; set; }
+    public int Id { get; set; }
     public virtual ICollection<Item> Items { get; set; }
     public virtual Person Orderer { get; set; }
 }
